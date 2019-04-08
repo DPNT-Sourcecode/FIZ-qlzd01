8750e58f781d08317e054fabbb52f105771653fc
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:08:58 +0000

    Mon Apr 08 12:08:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,26 +1,18 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
 
-    def initialize
-      @number
-    end
   def fizz_buzz(number)
-    if number (1..9999) do
-      if number % 15 == 0
-        return 'fizz buzz'
-      elsif number % 3 == 0
-        return "fizz"
-      elsif  
-        number % 5 == 0
-        return 'buzz'
-      else
-        return number
-      end
+
+    if number  % 15 == 0
+      return 'fizz buzz'
+    elsif number % 3 == 0
+      return "fizz"
+    elsif  
+      number % 5 == 0
+      return 'buzz'
+    else
+      return number
     end
-    else return "number is outside range"
   end
-  end
+
 end
-
-
-
