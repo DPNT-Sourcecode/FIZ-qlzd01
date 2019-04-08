8980a0be84a87b5674e8a490c16e6bf44990b223
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:56:38 +0000

    Mon Apr 08 12:56:38 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,10 +1,8 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
 
-  i = 0 
-
   def fizz_buzz(number)
-    1..9999.each do |number|
+ 
         if (number.to_s.split('').include?("5") || number % 5 == 0) &&
           (number.to_s.split('').include?("3") || number % 3 == 0)
           return 'fizz buzz'
@@ -15,25 +13,10 @@ class FizzBuzz
         else 
           return number
         end
-      end
-    end
   end
 
-
-#   def fizz_buzz(number)
-#     if (number.to_s.split('').include?("5") || number % 5 == 0) &&
-#       (number.to_s.split('').include?("3") || number % 3 == 0)
-#       return 'fizz buzz'
-#     elsif number.to_s.split('').include?("5") || number % 5 == 0
-#       return 'buzz'
-#     elsif number.to_s.split('').include?("3") || number % 3 == 0
-#       return "fizz"
-#     else 
-#       return number
-#     end
-# end
-
 end
+
 
 
 
