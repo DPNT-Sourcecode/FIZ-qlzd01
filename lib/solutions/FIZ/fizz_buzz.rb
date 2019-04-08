0c9e7b2726cc8759468f27a6b6613e91d79f3448
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:14:58 +0000

    Mon Apr 08 12:14:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,7 +2,7 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-
+  if number (1..9999) 
     if number  % 15 == 0
       return 'fizz buzz'
     elsif number % 3 == 0
@@ -13,6 +13,10 @@ class FizzBuzz
     else
       return number
     end
+  else
+    puts "number is out of range"
+  end
   end
 
 end
+
