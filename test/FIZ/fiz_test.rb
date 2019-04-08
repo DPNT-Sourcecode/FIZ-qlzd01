96ab256aa4f0af63ee88447acf2dc40829d2bfb2
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 14:34:19 +0000

    Mon Apr 08 14:34:19 UTC 2019

--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -10,6 +10,7 @@ class ClientTest < Minitest::Test
 
   def test_fiz
     assert_equal "deluxe", FizzBuzz.new.fizz_buzz(333), "App should print 'deluxe' when number is greater than 10 and all digits are identical"
+    assert_equal "fizz deluxe", FizzBuzz.new.fizz_buzz(222), "App should print 'fizz deluxe' when number is divisble by 3, has identical digits and is greater than 10"
     assert_equal "fizz", FizzBuzz.new.fizz_buzz(34), "App should print 'fizz' when number is divisble by 3 or contains 3"
     assert_equal "buzz", FizzBuzz.new.fizz_buzz(59), "App should print 'buzz' when is divisble by 5 or contains 5"
     assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(3553), "App should print 'fizz buzz' when number is divisible by 5 or 3 and contains both 3 and 5"
