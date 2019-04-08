5ebd76970e17cda0dbae1201a018d828f01bac94
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:59:40 +0000

    Mon Apr 08 12:59:40 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,19 +3,20 @@ class FizzBuzz
 
   def fizz_buzz(number)
  
-        if (number.to_s.split('').include?("5") || number % 5 == 0) &&
-          (number.to_s.split('').include?("3") || number % 3 == 0)
-          return 'fizz buzz'
-        elsif number.to_s.split('').include?("5") || number % 5 == 0
-          return 'buzz'
-        elsif number.to_s.split('').include?("3") || number % 3 == 0
-          return "fizz"
-        else 
-          return number
-        end
+      if (number.to_s.split('').include?("5") || number % 5 == 0) &&
+        (number.to_s.split('').include?("3") || number % 3 == 0)
+        return 'fizz buzz'
+      elsif number.to_s.split('').include?("5") || number % 5 == 0
+        return 'buzz'
+      elsif number.to_s.split('').include?("3") || number % 3 == 0
+        return "fizz"
+      else 
+        return number
+      end
   end
 
 end
+
 
 
 
--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -9,11 +9,12 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fiz
-    # assert_equal "fizz", FizzBuzz.new.fizz_buzz(34), "App should print 'fizz' when number is divisble by 3 or contains 3"
-    # assert_equal "buzz", FizzBuzz.new.fizz_buzz(5), "App should print 'buzz' when is divisble by 5 or contains 5"
+    assert_equal "fizz", FizzBuzz.new.fizz_buzz(34), "App should print 'fizz' when number is divisble by 3 or contains 3"
+    assert_equal "buzz", FizzBuzz.new.fizz_buzz(5), "App should print 'buzz' when is divisble by 5 or contains 5"
     assert_equal "fizz buzz", FizzBuzz.new.fizz_buzz(15), "App should print 'fizz buzz' when number is divisible by 5 or 3 and contains both 3 and 5"
   end
 
 
 end
+
 
