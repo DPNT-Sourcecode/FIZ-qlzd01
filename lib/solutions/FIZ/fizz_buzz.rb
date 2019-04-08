7aeff20e89633c5b2ded786f0db1f3e666c018f0
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 14:02:41 +0000

    Mon Apr 08 14:02:41 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -6,7 +6,12 @@ class FizzBuzz
   def fizz_buzz(number)
       # if (1..9999) === number
         arr = number.to_s.split('')
-            if (arr.all? {|digit| digit == arr[0]}) && number > 10
+
+          if (arr.all? {|digit| digit == arr[0]}) && number > 10) && 
+          ((arr.include?("5") || number % 5 == 0) &&
+                (arr.include?("3") || number % 3 == 0))
+                return "fizz buzz deluxe"
+            elsif (arr.all? {|digit| digit == arr[0]}) && number > 10
             @answer = "deluxe"
               return 'deluxe'
             elsif (arr.include?("5") || number % 5 == 0) &&
@@ -23,12 +28,6 @@ class FizzBuzz
               return number
             end
       
-
-
-
-
-
-
     #   else
     #   puts "Number out of range"
     # end
@@ -55,7 +54,3 @@ end
 #   puts "Number out of range"
 # end
 # end
-
-
-
-
--- test/FIZ/fiz_test.rb
+++ test/FIZ/fiz_test.rb
@@ -18,6 +18,3 @@ class ClientTest < Minitest::Test
 
 
 end
-
-
-
