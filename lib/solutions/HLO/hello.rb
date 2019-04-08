4cfb19896beb4fe7edf8df269307afda77b1abfc
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 10:55:30 +0000

    Mon Apr 08 10:55:30 UTC 2019

--- lib/solutions/HLO/hello.rb
+++ lib/solutions/HLO/hello.rb
@@ -7,4 +7,11 @@ class Hello
     return greeting
   end
 
+  def hello_friend(friend_name)
+    # raise 'Not implemented'
+    greeting_start = "Hello, "
+    greeting_end = " !"
+    return greeting_start + friend_name + greeting_end
+  end
+
 end
--- test/HLO/hlo_test.rb
+++ test/HLO/hlo_test.rb
@@ -8,12 +8,12 @@ require_solution 'HLO'
 
 class ClientTest < Minitest::Test
 
-  def test_hlo
-    assert_equal "Hello, World!", Hello.new.hello("Marc"), 'App should say Hello, World!'
-  end
+#   def test_hlo
+#     assert_equal "Hello, World!", Hello.new.hello("Marc"), 'App should say Hello, World!'
+#   end
 
   def test_hlo_r2
-    assert_equal "Hello, Marc!", Hello.new.hello("Marc"), 'App should say Hello, World!'
+    assert_equal "Hello, John!", Hello.new.hello("John"), 'App should say Hello, John!'
   end
 
 end
