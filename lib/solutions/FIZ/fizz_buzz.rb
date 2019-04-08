960639f3f1c8decfe798d3f19f06ce4933caa061
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:05:40 +0000

    Mon Apr 08 13:05:40 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -18,3 +18,23 @@ class FizzBuzz
     end
   end
 end
+
+
+
+# def fizz_buzz(number)
+#   if (1..9999) === number
+#         if (number.to_s.split('').include?("5") || number % 5 == 0) &&
+#           (number.to_s.split('').include?("3") || number % 3 == 0)
+#           return 'fizz buzz'
+#         elsif number.to_s.split('').include?("5") || number % 5 == 0
+#           return 'buzz'
+#         elsif number.to_s.split('').include?("3") || number % 3 == 0
+#           return "fizz"
+#         else 
+#           return number
+#         end
+#   else
+#   puts "Number out of range"
+# end
+# end
+
