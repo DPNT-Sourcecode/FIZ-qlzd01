8cacdf22847a78dd75a54d7e5e772db6120b4922
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 13:59:41 +0000

    Mon Apr 08 13:59:41 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,18 +1,23 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
 
+  @answer = ""
 
   def fizz_buzz(number)
       # if (1..9999) === number
         arr = number.to_s.split('')
             if (arr.all? {|digit| digit == arr[0]}) && number > 10
+            @answer = "deluxe"
               return 'deluxe'
             elsif (arr.include?("5") || number % 5 == 0) &&
                 (arr.include?("3") || number % 3 == 0)
+                @answer = "fizz buzz"
               return 'fizz buzz'
             elsif (arr.include?("5") || number % 5 == 0)
+              @answer = "buzz"
               return 'buzz'
             elsif (arr.include?("3") || number % 3 == 0)
+              @answer = "fizz"
               return "fizz"
             else 
               return number
@@ -50,6 +55,7 @@ end
 #   puts "Number out of range"
 # end
 # end
+
 
 
 
