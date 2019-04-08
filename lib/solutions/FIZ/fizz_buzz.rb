654de74737a4b112ce78f6be8526205eabc08e61
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:53:58 +0000

    Mon Apr 08 12:53:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -1,9 +1,10 @@
 # noinspection RubyUnusedLocalVariable
 class FizzBuzz
 
-  def fizz_buzz(number)
-    while i (1..9999) do
+  i = 0 
 
+  def fizz_buzz(number)
+    1..9999.each do |number|
         if (number.to_s.split('').include?("5") || number % 5 == 0) &&
           (number.to_s.split('').include?("3") || number % 3 == 0)
           return 'fizz buzz'
@@ -15,6 +16,7 @@ class FizzBuzz
           return number
         end
       end
+    end
   end
 
 
@@ -32,6 +34,7 @@ class FizzBuzz
 # end
 
 end
+
 
 
 
