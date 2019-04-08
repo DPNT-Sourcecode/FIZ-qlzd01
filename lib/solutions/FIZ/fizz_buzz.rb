388f270fd1d0a007d993fb6350689c405f104a79
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 14:37:19 +0000

    Mon Apr 08 14:37:19 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,28 +4,28 @@ class FizzBuzz
  GAME_RULES = {}
 
 
-   deluxe => ((arr.all? {|digit| digit == arr[0]}) && number > 10)
-   fizz buzz deluxe => ((arr.all? {|digit| digit == arr[0]}) && number > 10) &&
-   ((arr.include?("5") || number % 5 == 0) &&
-   (arr.include?("3") || number % 3 == 0))
-   fizz deluxe => (arr.all? {|digit| digit == arr[0]} && number > 10) && (arr.include?("3") || number % 3 == 0)
-   buzz deluxe =>((arr.all? {|digit| digit == arr[0]}) && number > 10) && (arr.include?("5") || number % 5 == 0)
-   fizz => (arr.include?("3") || number % 3 == 0)
-   buzz =>(arr.include?("5") || number % 5 == 0)
-   fizz buzz => (arr.include?("5") || number % 5 == 0) &&
-   (arr.include?("3") || number % 3 == 0)
+  #  deluxe => ((arr.all? {|digit| digit == arr[0]}) && number > 10)
+  #  fizz buzz deluxe => ((arr.all? {|digit| digit == arr[0]}) && number > 10) &&
+  #  ((arr.include?("5") || number % 5 == 0) &&
+  #  (arr.include?("3") || number % 3 == 0))
+  #  fizz deluxe => (arr.all? {|digit| digit == arr[0]} && number > 10) && (arr.include?("3") || number % 3 == 0)
+  # #  buzz deluxe =>((arr.all? {|digit| digit == arr[0]}) && number > 10) && (arr.include?("5") || number % 5 == 0)
+  #  fizz => (arr.include?("3") || number % 3 == 0)
+  #  buzz =>(arr.include?("5") || number % 5 == 0)
+  #  fizz buzz => (arr.include?("5") || number % 5 == 0) &&
+  #  (arr.include?("3") || number % 3 == 0)
 
 
   def fizz_buzz(number)
       if (1..9999) === number
         arr = number.to_s.split('')
-
-        if ((arr.all? {|digit| digit == arr[0]}) && number > 10) &&
-        ((arr.include?("5") || number % 5 == 0) &&
-        (arr.include?("3") || number % 3 == 0))
-        return "fizz buzz deluxe"
-        
-         elsif (arr.all? {|digit| digit == arr[0]}) && number > 10
+          if ((arr.all? {|digit| digit == arr[0]}) && number > 10) &&
+          ((arr.include?("5") || number % 5 == 0) &&
+          (arr.include?("3") || number % 3 == 0))
+          return "fizz buzz deluxe"
+          elsif (arr.all? {|digit| digit == arr[0]} && number > 10) && (arr.include?("3") || number % 3 == 0)
+          return "fizz deluxe"
+          elsif (arr.all? {|digit| digit == arr[0]}) && number > 10
               return 'deluxe'
             elsif (arr.include?("5") || number % 5 == 0) &&
                 (arr.include?("3") || number % 3 == 0)
@@ -43,3 +43,4 @@ class FizzBuzz
     end
   end
 end
+
