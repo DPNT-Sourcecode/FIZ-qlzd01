cbb14b7ad0cac24a38d3ef1f8ba18a917301831a
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 12:50:58 +0000

    Mon Apr 08 12:50:58 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,6 +2,8 @@
 class FizzBuzz
 
   def fizz_buzz(number)
+    while i (1..9999) do
+
         if (number.to_s.split('').include?("5") || number % 5 == 0) &&
           (number.to_s.split('').include?("3") || number % 3 == 0)
           return 'fizz buzz'
@@ -12,8 +14,24 @@ class FizzBuzz
         else 
           return number
         end
+      end
   end
 
+
+#   def fizz_buzz(number)
+#     if (number.to_s.split('').include?("5") || number % 5 == 0) &&
+#       (number.to_s.split('').include?("3") || number % 3 == 0)
+#       return 'fizz buzz'
+#     elsif number.to_s.split('').include?("5") || number % 5 == 0
+#       return 'buzz'
+#     elsif number.to_s.split('').include?("3") || number % 3 == 0
+#       return "fizz"
+#     else 
+#       return number
+#     end
+# end
+
 end
+
 
 
