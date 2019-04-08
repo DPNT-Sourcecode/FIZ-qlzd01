0982e26eb132dc8a0a67c9e58af72c92483a7764
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 14:59:03 +0000

    Mon Apr 08 14:59:03 UTC 2019

--- lib/solutions/FIZ/conditions.rb
+++ lib/solutions/FIZ/conditions.rb
@@ -4,29 +4,33 @@ class Conditions
         @number = number
     end
 
-    @arr = @number.to_s.split('')
+    # @arr = @number.to_s.split('')
 
     def is_buzz?(number)
+        @arr = @number.to_s.split('')
         if (@arr.include?("5") || number % 5 == 0)
             return true
         end
     end
 
     def is_fizz?(number)
+        @arr = @number.to_s.split('')
         if (@arr.include?("3") || number % 3 == 0)
             return true
         end
     end
 
     def is_fizz_buzz?(number)
-        if (arr.include?("5") || number % 5 == 0) &&
-            (arr.include?("3") || number % 3 == 0)
+        @arr = @number.to_s.split('')
+        if (@arr.include?("5") || number % 5 == 0) &&
+            (@arr.include?("3") || number % 3 == 0)
             return true
         end
     end
 
     def is_deluxe?(number)
-        if ((arr.all? {|digit| digit == arr[0]}) && number > 10)
+        @arr = @number.to_s.split('')
+        if ((@arr.all? {|digit| digit == @arr[0]}) && number > 10)
             return true
         end
     end
@@ -35,3 +39,4 @@ end
 
 
  
+
